9b0ff5e4f03925072a557c0ccf221183b8b8f761
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-10-15 13:18:54 +0530

    Add files via upload

---
+++ 3. Decimal to Hexadecimal.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 0, j = 0, num, hexadecimal[20];
+    printf("\nEnter decimal number: ");
+    scanf("%d", &num);
+
+    while(num > 0) {
+        hexadecimal[i] = num % 16;
+        i++;
+        num /= 16;
+    }
+
+    printf("Hexadecimal number is: ");
+    for(int j = i - 1; j >= 0; j--) {
+        switch(hexadecimal[j]) {
+            case 10:
+                printf("A");
+                break;
+
+            case 11:
+                printf("B");
+                break;
+
+            case 12:
+                printf("C");
+                break;
+
+            case 13:
+                printf("D");
+                break;
+
+            case 14:
+                printf("E");
+                break;
+
+            case 15:
+                printf("F");
+                break;
+
+            default:
+                printf("%d", hexadecimal[j]);
+        }
+    }
+    printf("\n\n");
+    return 0;
+}
